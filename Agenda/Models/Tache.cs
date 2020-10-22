184f6a224411447cfd4fa86b51082749fd2c29fc
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2020-10-22 13:40:41 -0400

    Validation - Data Annotations

--- Agenda/Models/Tache.cs
+++ Agenda/Models/Tache.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,25 @@ namespace Agenda.Models
     public class Tache
     {
         public int TacheId { get; set; }
+
+        [Required(ErrorMessage = "champs requis")]
+        [StringLength(50, ErrorMessage = "Nombre de caractères 50")]
         public string Title { get; set; }
+        
+        [Required(ErrorMessage = "champs requis")]
+        [StringLength(500, ErrorMessage = "Nombre de caractères 500")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "champs requis")]
+        [DataType(DataType.DateTime)]       
         public DateTime Debut { get; set; }
+
+        [Required(ErrorMessage = "champs requis")]
+        [DataType(DataType.DateTime)]
         public DateTime Fin { get; set; }
+
+        [Required(ErrorMessage = "champs requis")]
+        [StringLength(50, ErrorMessage = "Nombre de caractères 5")]
         public string Priorite { get; set; }
     }
 }
