5cf0530c927066dd7eadc27bbf66b837bc59b917
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2020-10-23 09:23:32 -0400

    Petit example: Entity FrameWork - Relationships, fluent API

---
+++ EntityFrameWorkRelationships/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkRelationships.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Personne> Gens { get; set; }
+
+        public Context(DbContextOptions<Context> dbContextOptions) :base (dbContextOptions)
+        {
+
+        }
+
+
+    }
+}
---
+++ EntityFrameWorkRelationships/Models/Personne.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameWorkRelationships.Models
+{
+    public class Personne
+    {
+        public int PersonneId { get; set; }
+        
+        public string Nom { get; set; }
+
+        public DateTime DateNaissance { get; set; }
+        
+        public double poids { get; set; }
+    }
+}
