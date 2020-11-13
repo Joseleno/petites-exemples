e138110e6888f7b5f5eb03b2840183367c53f821
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2020-11-13 15:07:24 -0500

    Le CRUD Type de Cours, modal materialize

--- GestionCV/Models/TypeCours.cs
+++ GestionCV/Models/TypeCours.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace GestionCV.Models
     public class TypeCours
     {
         public int TypeCoursId { get; set; }
+
+        [Required(ErrorMessage = "Champs requis")]
+        [StringLength(50, ErrorMessage = "Seulement 50 caractères")]
         public string Type { get; set; }
         public ICollection<Formation> Formations { get; set; }
 
